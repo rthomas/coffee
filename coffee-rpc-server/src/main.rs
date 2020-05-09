88435c8534b18ac29353895323a2c05874b7f081
Ryan Thomas <ryan@ryant.org>
2020-05-10 07:59:44 +1000

    Refactor of rpc server binary.
    Moved the rpc server guts out of the main file to try to clean it up a
    bit.

--- coffee-rpc-server/src/main.rs
+++ coffee-rpc-server/src/main.rs
@@ -1,14 +1,11 @@
-use coffee_common::coffee::coffee_server::{Coffee, CoffeeServer};
-use coffee_common::coffee::{
-    AddCoffeeRequest, AddCoffeeResponse, ApiKey, ListCoffeeRequest, ListCoffeeResponse,
-    RegisterRequest, RegisterResponse,
-};
+mod rpc;
+
+use coffee_common::coffee::coffee_server::CoffeeServer;
 use coffee_common::db::Db;
+use rpc::CoffeeService;
 
 use clap::{App, AppSettings, Arg};
-use crypto::digest::Digest;
-use crypto::sha1::Sha1;
-use tonic::{transport::Server, Request, Response, Status};
+use tonic::transport::Server;
 
 static DEFAULT_ADDR: &str = "[::1]:50051";
 static DEFAULT_DB: &str = "coffee_db";
@@ -60,9 +57,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = matches.value_of("addr").unwrap_or(DEFAULT_ADDR).parse()?;
     let db = matches.value_of("db").unwrap_or(DEFAULT_DB);
 
-    let coffee = CoffeeService {
-        db: Db::new(db).await?,
-    };
+    let coffee = CoffeeService::new(Db::new(db).await?);
 
     Server::builder()
         .add_service(CoffeeServer::new(coffee))
@@ -70,58 +65,4 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
 
     Ok(())
-}
-
-#[derive(Debug)]
-struct CoffeeService {
-    db: Db,
-}
-
-#[tonic::async_trait]
-impl Coffee for CoffeeService {
-    async fn register(
-        &self,
-        req: Request<RegisterRequest>,
-    ) -> Result<Response<RegisterResponse>, Status> {
-        // TODO: The actual registration flow - i.e. check if registered and return same api key, otherwise just return a new api key for them for now and store in the db.
-
-        // For now we will just use a sha1 hash of the email... not ideal but fine for now.
-
-        let mut hasher = Sha1::new();
-        hasher.input_str(&req.get_ref().email);
-
-        let resp = RegisterResponse {
-            success: true,
-            key: Some(ApiKey {
-                key: hasher.result_str(),
-            }),
-        };
-        Ok(Response::new(resp))
-    }
-
-    async fn add_coffee(
-        &self,
-        req: Request<AddCoffeeRequest>,
-    ) -> Result<Response<AddCoffeeResponse>, Status> {
-        println!("Adding coffee: {:#?}", req);
-
-        let resp = AddCoffeeResponse { success: true };
-
-        Ok(Response::new(resp))
-    }
-
-    async fn list_coffee(
-        &self,
-        req: Request<ListCoffeeRequest>,
-    ) -> Result<Response<ListCoffeeResponse>, Status> {
-        println!("Listing coffee: {:#?}", req);
-
-        let api_key = &req.get_ref().key.as_ref().unwrap().key;
-
-        println!("COFFEES: {:#?}", self.db.get_coffees(api_key).await?);
-
-        let resp = ListCoffeeResponse { coffees: vec![] };
-
-        Ok(Response::new(resp))
-    }
 }
