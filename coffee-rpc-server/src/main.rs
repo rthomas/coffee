ec59c63bed4e82d62b57dcbb001db62e63c33074
Ryan Thomas <ryan@ryant.org>
2020-05-09 22:17:51 +1000

    Reorder the fn definitions.

--- coffee-rpc-server/src/main.rs
+++ coffee-rpc-server/src/main.rs
@@ -31,6 +31,26 @@ struct CoffeeService {
 
 #[tonic::async_trait]
 impl Coffee for CoffeeService {
+    async fn register(
+        &self,
+        req: Request<RegisterRequest>,
+    ) -> Result<Response<RegisterResponse>, Status> {
+        // TODO: The actual registration flow - i.e. check if registered and return same api key, otherwise just return a new api key for them for now and store in the db.
+
+        // For now we will just use a sha1 hash of the email... not ideal but fine for now.
+
+        let mut hasher = Sha1::new();
+        hasher.input_str(&req.get_ref().email);
+
+        let resp = RegisterResponse {
+            success: true,
+            key: Some(ApiKey {
+                key: hasher.result_str(),
+            }),
+        };
+        Ok(Response::new(resp))
+    }
+
     async fn add_coffee(
         &self,
         req: Request<AddCoffeeRequest>,
@@ -56,26 +76,6 @@ impl Coffee for CoffeeService {
 
         let resp = ListCoffeeResponse { coffees: vec![] };
 
-        Ok(Response::new(resp))
-    }
-
-    async fn register(
-        &self,
-        req: Request<RegisterRequest>,
-    ) -> Result<Response<RegisterResponse>, Status> {
-        // TODO: The actual registration flow - i.e. check if registered and return same api key, otherwise just return a new api key for them for now and store in the db.
-
-        // For now we will just use a sha1 hash of the email... not ideal but fine for now.
-
-        let mut hasher = Sha1::new();
-        hasher.input_str(&req.get_ref().email);
-
-        let resp = RegisterResponse {
-            success: true,
-            key: Some(ApiKey {
-                key: hasher.result_str(),
-            }),
-        };
         Ok(Response::new(resp))
     }
 }
